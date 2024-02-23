8b16cae88382e598042560d1a3723bf173fb570c
AdeiLB <145749081+AdeiLB@users.noreply.github.com>
2024-02-23 08:50:52 +0100

    Namespace test

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,23 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour
-{
-    Enemy enemy;
-    // Start is called before the first frame update
-    void Start()
+    public class Main : MonoBehaviour
     {
-        enemy = new Enemy("Bob");
-        Player player = new Player("Mike");
-        Debug.Log("Hello git commit");
-        Debug.Log("Hello from class 105");
-      
-    }
+        Enemy enemy;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Soy la clase 1");
+            enemy = new Enemy("Bob");
+            Player player = new Player("Mike");
+            Debug.Log("Hello git commit");
+            Debug.Log("Hello from class 105");
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Enemy name " + enemy.name);
-        Debug.Log("Enemy HP" + enemy.Life);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("Enemy name " + enemy.name);
+            Debug.Log("Enemy HP" + enemy.Life);
+        }
     }
-}
---
+++ Assets/Main1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Main1 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("soy la clase 2");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("soy la clase 2");
+        }
+    }
+}
